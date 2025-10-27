fe9c3fa2cfe6c53d73d5d2152ac9ef1cc4c1fe1e
Alexi McNabb <amcnabb4@charlotte.edu>
2025-10-27 10:42:22 -0400

    scraped list of articles

---
+++ scrape_blog.py
@@ -0,0 +1,18 @@
+import bs4
+import requests
+import os
+
+blog_scrape = "https://www.acfe.com/acfe-insights-blog"
+raw_blog = requests.get(blog_scrape)
+soup_blog = bs4.BeautifulSoup(raw_blog.text, 'html.parser')
+
+with open("acfe_blog.txt", 'w', encoding='utf-8') as f:
+    f.write(str(soup_blog))
+
+articles = soup_blog.find_all("a", class_='color-secondary')
+article_links = ""
+for article in articles:
+    link = article.get('href')
+    article_links += link + "\n"
+with open("article_urls.txt", 'w', encoding='utf-8') as f:
+    f.write(str(article_links))
