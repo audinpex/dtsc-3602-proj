3c2c3c72801e8fb740003c89a9c8beb1e7fef7fc
Alexi McNabb <amcnabb4@charlotte.edu>
2025-11-03 14:03:53 -0500

    added keyword check code, exported valid articles to csv for storage

--- scrape_articles.py
+++ scrape_articles.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 urls = []
+FRAUD_KEYWORDS = ['fraud', 'scam', 'embezzlement', 'whistleblower', 'corruption', 'racketeering', 'transaction failure', 'irregularity', 'exposure', 'non-compliance', 'error']
 
 with open("article_urls.txt", 'r', encoding='utf-8') as f:
     for line in f:
@@ -16,7 +17,10 @@ for i in range(len(urls)):
 
 articles_df = pd.DataFrame(columns=['title','author','text'])
 
-for i in range(20):
+articles_found = 0
+
+for i in range(len(urls)):
+    keywords_found = 0
     current_url = urls[i]
     raw_article = requests.get(current_url)
     souped_article = bs4.BeautifulSoup(raw_article.text, 'html.parser')
@@ -26,11 +30,17 @@ for i in range(20):
     article_title = souped_article.find('h1')
     article_author = souped_article.find('h5', class_='margin-top-1')
     article_body = souped_article.find('div', class_="cell large-8")
-    article_write = article_title.text + '\n' + article_author.text + '\n' + article_body.text
-    file_name_2 = 'article-text-' + str(i) + '.txt'
+    for keyword in FRAUD_KEYWORDS:
+        if keyword in str(article_body):
+            keywords_found += 1
+    #article_write = article_title.text + '\n' + article_author.text + '\n' + article_body.text
+    #file_name_2 = 'article-text-' + str(i) + '.txt'
     #with open(file_name_2, 'w', encoding='utf-8') as f:
     #    f.write(str(article_write))
-    curr_article = pd.DataFrame({'title':[article_title], 'author':[article_author], 'text':[article_body]})
-    articles_df = pd.concat([articles_df, curr_article])
+    if keywords_found > 0:
+        articles_found += 1
+        curr_article = pd.DataFrame({'title':[article_title], 'author':[article_author], 'text':[article_body]})
+        articles_df = pd.concat([articles_df, curr_article], ignore_index=True)
 
-print(articles_df)
+articles_df.to_csv('articles.csv')
+print(articles_found)
