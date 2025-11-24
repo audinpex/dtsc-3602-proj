9897c51cc4c5f7b40b9a516fc82181ffa466981c
Alexi McNabb <amcnabb4@charlotte.edu>
2025-11-24 13:53:51 -0500

    added requirements.txt, started code comments

--- scrape_blog.py
+++ scrape_blog.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import os
 
+# Gets html of main blog page
 blog_scrape = "https://www.acfe.com/acfe-insights-blog"
 raw_blog = requests.get(blog_scrape)
 soup_blog = bs4.BeautifulSoup(raw_blog.text, 'html.parser')
@@ -9,6 +10,7 @@ soup_blog = bs4.BeautifulSoup(raw_blog.text, 'html.parser')
 with open("acfe_blog.txt", 'w', encoding='utf-8') as f:
     f.write(str(soup_blog))
 
+# Identifies links to blog articles and writes them to a text file
 articles = soup_blog.find_all("a", class_='color-secondary')
 article_links = ""
 for article in articles:
